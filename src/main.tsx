6cb2a9c6fd941077e4572ad600a9770a5f09bccd
Sina Khaninejad <sinakaya98765@gmail.com>
2023-09-03 02:39:38 +0330

    change main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
